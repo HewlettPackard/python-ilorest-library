00f005c7c12333975e75ad1ed778294b6514c926
mridul-ganga <mridul-ganga@hpe.com>
2019-05-14 23:32:30 +0530

    making use of Redfish._redfishobject for the calls

--- examples/quickstart_redfish.py
+++ examples/quickstart_redfish.py
@@ -1,5 +1,5 @@
 import sys
-import redfish
+from Redfish._redfishobject import RedfishObject
 
 # When running on the server locally use the following commented values
 # iLO_host = "blobstore://."
@@ -8,22 +8,18 @@ import redfish
 
 # When running remotely connect using the iLO address, iLO account name, 
 # and password to send https requests
-iLO_host = "https://10.0.0.100"
-login_account = "admin"
-login_password = "password"
+iLO_https_url = "https://10.0.0.100"
+iLO_account = "admin"
+iLO_password = "password"
 
 ## Create a REDFISH object
-REDFISH_OBJ = redfish.redfish_client(base_url=iLO_host,username=login_account, \
-                          password=login_password)
-
-# Login into the server and create a session
-REDFISH_OBJ.login(auth="session")
+REDFISH_OBJ = RedfishObject(iLO_https_url, iLO_account, iLO_password)
 
 # Do a GET on a given path
-response = REDFISH_OBJ.get("/redfish/v1/systems/1", None)
+response = REDFISH_OBJ.redfish_get('/rest/v1/systems/1')
 
 # Print out the response
 sys.stdout.write("%s\n" % response)
 
 # Logout of the current session
-REDFISH_OBJ.logout()
+REDFISH_OBJ.redfish_client.logout()
